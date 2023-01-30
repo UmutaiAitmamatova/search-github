0fa4c481610f4e91ed68b83e1383517a27fa113c
Umutai <umutaiaitmamativa@gmail.com>
2023-01-30 13:56:43 +0600

    added component Repositories and Mypage

--- src/components/MyPage/index.js
+++ src/components/MyPage/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Repositories from '../Repositories';
 import UserInfo from '../UserInfo';
 import classes from './MyPage.module.scss'
 
@@ -6,8 +7,21 @@ function MyPage() {
   return (
     <div className={classes.MyPage}>
         <div className={classes.container}>
-            <UserInfo/>
+            <div className={classes.content}>
+                <div className={classes.right}>
+                    <UserInfo/>
+                </div>
+
+                <div className={classes.left}>
+                  <h2>Repositories</h2>
+                  
+                  <Repositories/>
+                  <Repositories/>
+                  <Repositories/>
+                </div>
+            </div>
         </div>
+
     </div>
   )
 }
---
+++ src/components/Repositories/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Repositories.module.scss';
+
+function Repositories() {
+  return (
+    <div className={classes.repositories}>
+        <p className={classes.title}>mypage</p>
+    </div>
+  )
+}
+
+export default Repositories;
